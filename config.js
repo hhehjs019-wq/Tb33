dd2539008740030427e11d0cd381ca35072cd748
hhehjs019-wq <hhehjs019@gmail.com>
2025-11-20 07:42:57 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  YOUTUBE_API_KEY: "AIzaSyDsfmu3uSbvQfqdXWdj8I_WoSoY2BQW-dg",
+  MONGO_URI: "mongodb+srv://hhehjs019_db_user:egf8WahqMsC4B0e8@cluster0.clxxaia.mongodb.net/?appName=Cluster0"
+};
---
+++ server.js
@@ -0,0 +1,69 @@
+
+const express = require("express");
+const axios = require("axios");
+const cron = require("node-cron");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const config = require("./config");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(config.MONGO_URI);
+let db;
+
+async function connectDB() {
+  await client.connect();
+  db = client.db("youtube_tracker");
+  console.log("MongoDB connected");
+}
+connectDB();
+
+async function fetchStats(videoId) {
+  try {
+    const url = `https://www.googleapis.com/youtube/v3/videos?part=statistics&id=${videoId}&key=${config.YOUTUBE_API_KEY}`;
+    const res = await axios.get(url);
+    const stats = res.data.items[0].statistics;
+    return {
+      views: Number(stats.viewCount),
+      likes: Number(stats.likeCount || 0),
+      timestamp: new Date()
+    };
+  } catch (err) {
+    console.error("API Error:", err.message);
+    return null;
+  }
+}
+
+cron.schedule("*/5 * * * *", async () => {
+  console.log("Running 5-minute cron job...");
+  const videos = await db.collection("videos").find().toArray();
+
+  for (const v of videos) {
+    const stats = await fetchStats(v.videoId);
+    if (stats) {
+      await db.collection("history").insertOne({
+        videoId: v.videoId,
+        ...stats
+      });
+      console.log("Stored stats for:", v.videoId);
+    }
+  }
+});
+
+app.post("/start", async (req, res) => {
+  const { videoId } = req.body;
+  await db.collection("videos").updateOne(
+    { videoId },
+    { $set: { videoId } },
+    { upsert: true }
+  );
+  res.json({ message: "Tracking started", videoId });
+});
+
+app.get("/", (req, res) => {
+  res.send("YouTube Tracker Backend Running");
+});
+
+app.listen(10000, () => console.log("Server running on port 10000"));
